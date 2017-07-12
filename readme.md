b6c13e21d51804f3bb8c2bf255ec3037564a2267
Colin Germain <cgermain@gmail.com>
2017-07-12 08:47:55 -0400

    Added spawning of subprocesses equal to the number of CPU cores.

--- readme.md
+++ readme.md
@@ -8,6 +8,8 @@ The /dist/rtconvert.exe executable file was created with py2exe.  Microsoft's [V
 
 ## Usage
 
-GUI: Drag and drop an XML file onto the rtconvert.exe application
+GUI: Drag and drop an XML file or directory of XML onto the rtconvert.exe application
 
 Command Line: python rtconvert.py filename.xml
+
+If converting a directory of files, RTConvert will spawn a number of subprocesses equal to the number of CPU cores for multiprocessing.
--- rtconvert.py
+++ rtconvert.py
@@ -1,51 +1,57 @@
 import re
 import os
 import sys
+import time
+import tqdm
+from multiprocessing import Pool
+from multiprocessing import cpu_count
+from multiprocessing import freeze_support
+
 
 RTINSECONDS = "(.*)(RTINSECONDS=)(\d{0,})(.*)"
 RT = "(<group id=.*rt=\")(\d{0,})(.*)"
 
 def main(in_arg):
+	start_time = time.time()
+
 	#convert a single file
-	if os.path.isfile(in_arg):
+	if os.path.isfile(in_arg) and in_arg.endswith(".xml"):
+		print "Converting: " + str(in_arg)
 		convert_rt(in_arg)
 		print "\nFinished!\n"
+		print "Time: " + str(time.time() - start_time) + " seconds"
+
 	#convert all the .XML files in a directory
 	elif os.path.isdir(in_arg):
-		print "Processing Directory: " + in_arg + "\n"
-		for in_filename in get_absolute_file_paths(in_arg):
-			if in_filename.endswith(".xml"):
-				#if there is a problem with the conversion, quit
-				if not convert_rt(in_filename):
-					return
+		in_files = [file for file in get_absolute_file_paths(in_arg) if file.endswith(".xml")]
+		print "Converting " + str(len(in_files)) + " files:"
+		
+		#spawn subprocesses equal to the number of CPU cores 
+		pool = Pool(processes=cpu_count())
+		for _ in tqdm.tqdm(pool.imap_unordered(convert_rt, in_files), total=len(in_files), bar_format="{l_bar}{bar}| {n_fmt}/{total_fmt}"):
+			pass
+
 		print "\nFinished!\n"
+		print "Time: " + str(time.time() - start_time) + " seconds"
+
 	else:
 		print "File or directory not found."
-	
 
 def convert_rt(in_filename):
-	out_filename = create_out_filename_from_in_filename(in_filename)
-
-	if os.path.isfile(out_filename):
-		print "Output file for " + in_filename + " already exists in this location."
-		return False
-
 	regex_rt_in_seconds = re.compile(RTINSECONDS)
 	regex_rt = re.compile(RT)
 
-	total_line_count = buffered_line_count(in_filename)
-	#print "total line count: " + str(total_line_count)
-	current_line_count = 0
+	with open(in_filename) as in_file:
+		in_file_lines = list(in_file)
 
-	#print "Converting " + in_filename + "\n" 
-
-	with open(in_filename) as in_file, open(out_filename, "w+") as out_file:
+	out_filename = create_out_filename_from_in_filename(in_filename)
+	with open(out_filename, "w+") as out_file:	
 		previous_percentage_complete = 0
-		for line in in_file:
-			current_line_count+=1
+		for line in in_file_lines:
+			rt_in_seconds_result = re.search(regex_rt_in_seconds, line)
+			rt_result = re.search(regex_rt, line)
 
 			#check if the line is RTINSECONDS
-			rt_in_seconds_result = re.search(regex_rt_in_seconds, line)
 			if rt_in_seconds_result:
 				rt_prefix = rt_in_seconds_result.group(1)
 				rt_in_seconds_value = rt_in_seconds_result.group(3)
@@ -56,11 +62,9 @@ def convert_rt(in_filename):
 					out_file.write(rt_prefix + "RTINMINUTES=" + rt_in_minutes_value + rt_suffix + "\n")
 				else:
 					out_file.write(line)
-				continue
 
 			#check if the line has an rt value
-			rt_result = re.search(regex_rt, line)
-			if rt_result:
+			elif rt_result:
 				rt_prefix = rt_result.group(1)
 				rt_value = rt_result.group(2)
 				rt_suffix = rt_result.group(3)
@@ -70,23 +74,10 @@ def convert_rt(in_filename):
 					out_file.write(rt_prefix + rt_value_in_minutes + rt_suffix + "\n")
 				else:
 					out_file.write(line)
-				continue
 
 			# if not modifying a line, just write it out
 			else:
 				out_file.write(line)
-
-			percentage_complete = 100*current_line_count/total_line_count
-
-			#print "current line: " + str(current_line_count)
-			#print "percent complete: " + str(percentage_complete)
-			if percentage_complete > previous_percentage_complete:
-				previous_percentage_complete = percentage_complete
-				print "total lines: " + str(total_line_count) + "  |  current line: " + str(current_line_count) + "  |  percentage complete: " + str(percentage_complete) + "%\r",
-				sys.stdout.flush()
-	
-	print "Converted " + in_filename + " to " + out_filename
-	return True
 
 def create_out_filename_from_in_filename(in_filename):
 	dot_index = in_filename.index(".")
@@ -97,24 +88,12 @@ def get_absolute_file_paths(in_directory):
 	for root, dirs, files in os.walk(os.path.abspath(in_directory)):
 		return [os.path.join(root, file) for file in files]
 
-def buffered_line_count(filename):
-    f = open(filename)                  
-    lines = 0
-    buf_size = 1024 * 1024
-    read_f = f.read # loop optimization
-
-    buf = read_f(buf_size)
-    while buf:
-        lines += buf.count('\n')
-        buf = read_f(buf_size)
-
-    return lines\
-
 #takes a string and returns a string w/ 2 decimal places
 def convert_seconds_to_minutes(seconds):
 	return "%.2f" % (float(seconds)/60)
 
 if __name__ == "__main__":
+	freeze_support()
 	if len(sys.argv) == 2:
 		main(sys.argv[1])
 	else:
